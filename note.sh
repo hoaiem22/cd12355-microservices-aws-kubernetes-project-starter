daf46d90694b23654807186b0dd41706b9d31b4b
HoaiEm <hoaiem.heli22@gmail.com>
2024-07-04 14:47:14 +0700

    [feat] Update project

--- note.sh
+++ note.sh
@@ -8,11 +8,21 @@ psql -U coworking -d coworking
 kubectl port-forward service/postgresql-service 5433:5432 &
 ps aux | grep 'kubectl port-forward' | grep -v grep | awk '{print $2}' | xargs -r kill
 
+PGPASSWORD="$DB_PASSWORD" psql --host 127.0.0.1 -U coworking -d coworking -p 5433
+
 python3 -m venv venv  
 source venv/bin/activate
 pip3 install -r requirements.txt
 deactivate
 rm -rf venv
+
+
+export DB_USERNAME=coworking
+export DB_PASSWORD=${POSTGRES_PASSWORD}
+export DB_HOST=127.0.0.1
+export DB_PORT=5433
+export DB_NAME=coworking
+python app.py
 
 curl 127.0.0.1:5153/api/reports/daily_usage
 curl 127.0.0.1:5153/api/reports/user_visits
@@ -25,3 +35,11 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 docker build -t coworking .
 docker tag coworking:latest 536577976564.dkr.ecr.us-east-1.amazonaws.com/coworking:latest
 docker push 536577976564.dkr.ecr.us-east-1.amazonaws.com/coworking:latest
+
+curl aabe0102f83134eb0bbf035a56ed2a2f-1463734127.us-east-1.elb.amazonaws.com:5153/api/reports/daily_usage
+
+aws iam attach-role-policy \
+--role-name eksctl-coworking-cluster-nodegroup-NodeInstanceRole-t4n9402e2NWl \
+--policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy
+
+aws eks create-addon --addon-name amazon-cloudwatch-observability --cluster-name coworking-cluster
